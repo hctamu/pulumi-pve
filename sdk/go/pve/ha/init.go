45ed0c8c5522654b171e4b12e21c75ee6ea91cfa
Tamas Janosi <janosi96@gmail.com>
2025-05-27 14:09:19 +0000

    feat: add all sdk builds

--- sdk/go/pve/ha/init.go
+++ sdk/go/pve/ha/init.go
@@ -7,8 +7,8 @@ import (
 	"fmt"
 
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/hctamu/pulumi-pve/sdk/go/pve/internal"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 type module struct {
