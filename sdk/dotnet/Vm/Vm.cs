76b64ec66f33e309efc5cc9c1c290823d7b2d143
Tamas Janosi <janosi96@gmail.com>
2025-05-29 12:34:17 +0000

    feat: add build.yml to CI

--- sdk/dotnet/Vm/Vm.cs
+++ sdk/dotnet/Vm/Vm.cs
@@ -6,8 +6,9 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Pulumi.Serialization;
+using Pulumi;
 
-namespace Pulumi.Pve.Vm
+namespace Hctamu.Pve.Vm
 {
     [PveResourceType("pve:vm:Vm")]
     public partial class Vm : global::Pulumi.CustomResource
