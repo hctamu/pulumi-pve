9c2f0f373294326c2cbf642f2ba79977b9ad11cb
Daniel Hilfer <danielhilfer@outlook.de>
2025-08-08 14:01:18 +0200

    chore: some more trivial linting fixes

--- provider/pkg/provider/resources/storage/storage.go
+++ provider/pkg/provider/resources/storage/storage.go
@@ -1,3 +1,18 @@
+// Copyright 2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package storage provides Pulumi resources for managing files in Proxmox datastores.
 package storage
 
 import (
@@ -29,11 +44,13 @@ type FileInput struct {
 type File struct {
 }
 
+// FileSourceRaw represents the raw source data for a file upload.
 type FileSourceRaw struct {
 	FileData string `pulumi:"fileData"`
 	FileName string `pulumi:"fileName"`
 }
 
+// FileOutput represents the output properties of a file resource.
 type FileOutput struct {
 	FileInput
 }
