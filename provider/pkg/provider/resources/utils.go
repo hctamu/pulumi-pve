bcfbc4bff5efd82082e8cca644d0bf9d73f97bec
Mrton0121 <gombimarci@gmail.com>
2025-11-12 14:53:16 +0000

    fix: remove double declarated function after rebase

--- provider/pkg/provider/resources/utils.go
+++ provider/pkg/provider/resources/utils.go
@@ -55,17 +55,6 @@ func EndsWithLetter(str string) bool {
 	return unicode.IsLetter(lastChar)
 }
 
-// GetSortedMapKeys returns the keys of a map as a slice in no particular order.
-func GetSortedMapKeys[K cmp.Ordered, V any](inMap map[K]V) []K {
-	keys := make([]K, 0, len(inMap))
-	for key := range inMap {
-		keys = append(keys, key)
-	}
-
-	slices.Sort(keys)
-	return keys
-}
-
 // SliceToString is used to convert a slice of strings to a comma-separated string
 func SliceToString(slice []string) string {
 	if len(slice) == 0 {
