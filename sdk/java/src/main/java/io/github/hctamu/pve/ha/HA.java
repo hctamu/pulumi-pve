826347f757ea91d01b0ac5316f1b82504024d980
Tamas Janosi <janosi96@gmail.com>
2026-04-28 13:03:23 +0000

    chore: update workflow actions
    Signed-off-by: Tamas Janosi <janosi96@gmail.com>

--- sdk/java/src/main/java/io/github/hctamu/pve/ha/HA.java
+++ sdk/java/src/main/java/io/github/hctamu/pve/ha/HA.java
@@ -103,13 +103,10 @@ public class HA extends com.pulumi.resources.CustomResource {
     private static com.pulumi.resources.CustomResourceOptions makeResourceOptions(@Nullable com.pulumi.resources.CustomResourceOptions options, @Nullable Output<java.lang.String> id) {
         var defaultOptions = com.pulumi.resources.CustomResourceOptions.builder()
             .version(Utilities.getVersion())
-<<<<<<< HEAD
             .pluginDownloadURL("github://api.github.com/hctamu/pulumi-pve")
-=======
             .replaceOnChanges(List.of(
                 "resourceId"
             ))
->>>>>>> 2937458 (chore: bump dependency versions)
             .build();
         return com.pulumi.resources.CustomResourceOptions.merge(defaultOptions, options, id);
     }
